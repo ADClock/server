1f6ec073bbb93259815a5c66326a09ebfb3588c0
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-03-05 22:06:36 +0100

    made short name of HandType usable

--- src/model/HandType.kt
+++ src/model/HandType.kt
@@ -1,5 +1,10 @@
 package com.adclock.model
 
-enum class HandType(short: String) {
-    HOUR("H"), MINUTE("M");
+enum class HandType(val short: Char) {
+    HOUR('H'), MINUTE('M');
+
+    companion object {
+        val shortNames = values().map { it.short }
+        fun valueOfShort(short: Char) = values().single { it.short == short }
+    }
 }
