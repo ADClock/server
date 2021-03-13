1b2db0dffc8fcc580fe5073ad51dc77ca13cf678
Paul Maximilian Krüger <paul.krueger@alumni.fh-aachen.de>
2021-03-13 15:03:20 +0100

    added basic tests for task service #2

---
+++ test/services/TaskServiceTest.kt
@@ -0,0 +1,40 @@
+package com.adclock.services
+
+import com.adclock.instructionset.instructions.wall.RunInstruction
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TaskServiceTest {
+
+    @Test
+    fun shouldReturnEmptyTaskNameList() {
+        val service = TaskService()
+        assertEquals(emptyList(), service.getTaskNameList(), "Task list")
+    }
+
+    @Test
+    fun shouldReturnOneTaskNameList() {
+        val service = TaskService()
+        service.createTask("Foo")
+        assertEquals(listOf("Foo"), service.getTaskNameList(), "Task list")
+    }
+
+    @Test
+    fun addTaskWithNoInstructions() {
+        val service = TaskService()
+        service.createTask("Foo")
+        val task = service.getTask("Foo")
+        assertEquals("Foo", task.name, "Task name")
+        assertEquals(0, task.instructions.size, "Task instructions list size")
+    }
+
+
+    @Test
+    fun addTaskWithSingleInstruction() {
+        val service = TaskService()
+        service.createTask("Simple", listOf("RUN"))
+        val task = service.getTask("Simple")
+        assertEquals(1, task.instructions.size, "Task instructions list size")
+        assertEquals(RunInstruction::class, task.instructions.first()::class, "First task instruction")
+    }
+}
